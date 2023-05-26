335ee00047f9299b4dc982792a0e457d8e9aec53
Noam Bahar <noambahar2002@gmail.com>
2023-05-26 19:04:48 +0300

    temp feat: add slider value display

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -9,12 +9,16 @@ interface ISliderProps {
 export default function Slider({ min, max, state }: ISliderProps) {
   const [value, setValue] = useRecoilState(state);
   return (
-    <input
-      type='range'
-      min={min}
-      max={max}
-      value={value}
-      onChange={(e) => setValue(parseInt(e.target.value))}
-    />
+    <div style={{ display: 'flex' }}>
+      <input
+        style={{ width: '100%' }}
+        type='range'
+        min={min}
+        max={max}
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+      <output>{value}</output>
+    </div>
   );
 }
