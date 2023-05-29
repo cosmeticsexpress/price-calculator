5a0d70e3cd4a137db819cbfca720996286fe8214
Noam Bahar <noambahar2002@gmail.com>
2023-05-29 21:39:06 +0300

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily } from 'tailwindcss/defaultTheme';
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"FbSpoiler Regular"', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
