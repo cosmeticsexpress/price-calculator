0eb543ff0c04fcea5be9deeb4f07b3317d63c735
Noam Bahar <noambahar2002@gmail.com>
2023-05-31 20:34:56 +0300

    temp fix thumb jumping in working hour sliders

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { useRecoilState, RecoilState } from 'recoil';
 import { Range, getTrackBackground, Direction } from 'react-range';
 
@@ -14,62 +14,56 @@ export default function Slider({
   min,
   max,
   state,
-  className = '',
   outputRenderer = (value) => value,
 }: ISliderProps) {
   const [value, setValue] = useRecoilState(state);
-  const output = outputRenderer(value);
+  const [localValue, setLocalValue] = useState(value);
+  const output = outputRenderer(localValue);
+  useEffect(() => setLocalValue(value), [value]);
   return (
-    <>
-      <Range
-        rtl
-        values={[value]}
-        min={min}
-        max={max}
-        onChange={([value]) => setValue(value)}
-        renderTrack={({ props, children }) => (
+    <Range
+      rtl
+      values={[localValue]}
+      min={min}
+      max={max}
+      onChange={([newValue]) => {
+        setLocalValue(newValue);
+      }}
+      onFinalChange={([newValue]) => {
+        setValue(newValue);
+      }}
+      step={1}
+      renderTrack={({ props, children }) => (
+        <div
+          className='flex w-full'
+          onMouseDown={props.onMouseDown}
+          onTouchStart={props.onTouchStart}
+          style={{ ...props.style }}
+        >
           <div
-            className='flex w-full'
-            onMouseDown={props.onMouseDown}
-            onTouchStart={props.onTouchStart}
-            style={{ ...props.style }}
+            dir='rtl'
+            ref={props.ref}
+            className='border-2 rounded-full h-3 w-full self-center'
+            style={{
+              background: getTrackBackground({
+                direction: Direction.Left,
+                values: [localValue],
+                colors: ['#e6c462, #876628', '#fff'],
+                min,
+                max,
+              }),
+            }}
           >
-            <div
-              dir='rtl'
-              ref={props.ref}
-              className='border-2 rounded-full h-3 w-full self-center'
-              style={{
-                background: getTrackBackground({
-                  direction: Direction.Left,
-                  values: [value],
-                  colors: ['#e6c462, #876628', '#fff'],
-                  min,
-                  max,
-                }),
-              }}
-            >
-              {children}
-            </div>
+            {children}
           </div>
-        )}
-        renderThumb={({ props }) => (
-          <div {...props} style={props.style} className='relative'>
-            <div className='absolute bottom-3 w-28'>{output}</div>
-            <div className='bg-gold-900 w-4 aspect-square rounded-full' />
-          </div>
-        )}
-      />
-      {/* <input
-        className={[className, 'accent-gold-500 z-10'].join(' ').trim()}
-        type='range'
-        min={min}
-        max={max}
-        value={value}
-        onChange={(e) => setValue(parseInt(e.target.value))}
-      />
-      <output className='absolute left-0 -top-3.5 text-sm select-none'>
-        {outputRenderer(value)}
-      </output> */}
-    </>
+        </div>
+      )}
+      renderThumb={({ props }) => (
+        <div {...props} style={props.style} className='relative'>
+          <div className='absolute bottom-3 w-28'>{output}</div>
+          <div className='bg-gold-900 w-4 aspect-square rounded-full' />
+        </div>
+      )}
+    />
   );
 }
