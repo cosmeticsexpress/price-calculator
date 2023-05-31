191f95077270094039be85f4ba04239a5fe68f86
Noam Bahar <noambahar2002@gmail.com>
2023-05-31 22:29:14 +0300

    fix 2 tight gap between sliders

--- src/components/SliderGroup.tsx
+++ src/components/SliderGroup.tsx
@@ -12,9 +12,9 @@ interface ISliderGroupProps {
 
 export default function SliderGroup({ sliderProps }: ISliderGroupProps) {
   return (
-    <div className='flex flex-col gap-2 w-full'>
+    <div className='flex flex-col gap-6 w-full'>
       {sliderProps.map(({ label, ...slider }) => (
-        <div className='flex gap-1 relative' key={crypto.randomUUID()}>
+        <div className='flex gap-4 relative' key={crypto.randomUUID()}>
           <span className='text-xs text-center w-1/3'>{label}</span>
           <Slider className='col-span-3 w-2/3' {...slider} />
         </div>
