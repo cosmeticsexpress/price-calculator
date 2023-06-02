31b4278d5d4cbac888b0c20ac173dcdbc80f8272
Noam Bahar <noambahar2002@gmail.com>
2023-06-02 20:31:42 +0300

    dev feat: change title in local runs

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 import App from './App.tsx';
 import './index.css';
-import { RecoilRoot } from 'recoil';
+
+if (window.location.port) {
+  if (location.hostname === 'localhost')
+    document.title = 'localhost | Cosmetics Express';
+  else if (
+    location.hostname === '192.168.1.222' ||
+    location.hostname === '192.168.128.1'
+  )
+    document.title = 'local network | Cosmetics Express';
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
