77bbe93d19658015b2199dfe98d8e70941396753
Noam Bahar <noambahar2002@gmail.com>
2023-06-17 01:35:26 +0300

    good stuff

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,17 @@ import { RecoilRoot } from 'recoil';
 import App from './App.tsx';
 import './index.css';
 
+const css = (from: string, to: string) =>
+  `background: linear-gradient(to right, ${from}, ${to});
+  color: white; 
+  border-radius: 2em;
+  padding: 0.5em;
+  font-size: 6em; 
+  font-weight: bold;
+  font-family: ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji";`;
+console.log(`%cCosmetics Express`, css('#e6c462', '#876628'));
+console.log(`%cנעם בכר`, css('steelblue', 'lightcoral'));
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
