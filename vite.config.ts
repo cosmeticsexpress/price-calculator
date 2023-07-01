055d03bb4de4e4e7d8fa88a92dee1d299112a34b
Noam Bahar <noambahar2002@gmail.com>
2023-07-02 00:26:47 +0300

    fix: manifest.json not appearing in dev run separate web manifest from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { VitePWA } from 'vite-plugin-pwa';
+import { ManifestOptions, VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,45 +17,8 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifestFilename: 'manifest.webmanifest',
-      manifest: {
-        name: 'Cosmetics Express Price Calculator',
-        short_name: 'Price Calculator',
-        icons: [
-          {
-            src: '/price-calculator/icons/android-launchericon-512-512.png',
-            sizes: '512x512',
-            purpose: 'maskable',
-          },
-          {
-            src: '/price-calculator/icons/android-launchericon-192-192.png',
-            sizes: '192x192',
-          },
-          {
-            src: '/price-calculator/icons/android-launchericon-144-144.png',
-            sizes: '144x144',
-          },
-          {
-            src: '/price-calculator/icons/android-launchericon-96-96.png',
-            sizes: '96x96',
-          },
-          {
-            src: '/price-calculator/icons/android-launchericon-72-72.png',
-            sizes: '72x72',
-          },
-          {
-            src: '/price-calculator/icons/android-launchericon-48-48.png',
-            sizes: '48x48',
-          },
-        ],
-        start_url: './price-calculator/',
-        display: 'standalone',
-        background_color: 'white',
-        theme_color: 'white',
-        orientation: 'portrait',
-        lang: 'he',
-        description: 'מחשבון רווחים לטיפולי לייזר',
-      },
+      manifestFilename: 'manifest.json',
+      manifest: manifest as Partial<ManifestOptions>,
     }),
   ],
   base: '/price-calculator/',
